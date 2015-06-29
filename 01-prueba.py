cd998454ba5e720a06dc6de219cc9e5eaaf3d4c3
Gary dell3735 <inturiasgary@gmail.com>
2015-06-29 00:33:04 -0400

    ter

---
+++ 01-prueba.py
@@ -0,0 +1,45 @@
+go http://localhost:8000/calculo/
+
+fv 1 elementos "7"
+fv 1 nodos "8"
+submit aceptar
+
+showforms
+setglobal elemento1i "1"
+setglobal elemento1f "2"
+
+setglobal elemento2i "2"
+setglobal elemento2f "3"
+
+setglobal elemento3i "2"
+setglobal elemento3f "4"
+
+setglobal elemento4i "4"
+setglobal elemento4f "5"
+
+setglobal elemento5i "4"
+setglobal elemento5f "6"
+
+setglobal elemento6i "6"
+setglobal elemento6f "7"
+
+setglobal elemento7i "6"
+setglobal elemento7f "8"
+fv 1 elemento1i "${elemento1i}"
+fv 1 elemento1f "${elemento1f}"
+fv 1 elemento2i "${elemento2i}"
+fv 1 elemento2f "${elemento2f}"
+fv 1 elemento3i "${elemento3i}"
+fv 1 elemento3f "${elemento3f}"
+fv 1 elemento4i "${elemento4i}"
+fv 1 elemento4f "${elemento4f}"
+fv 1 elemento5i "${elemento5i}"
+fv 1 elemento5f "${elemento5f}"
+fv 1 elemento6i "${elemento6i}"
+fv 1 elemento6f "${elemento6f}"
+fv 1 elemento7i "${elemento7i}"
+fv 1 elemento7i "${elemento7f}"
+
+submit
+showlinks
+
